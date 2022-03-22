d71129d1240462d5738e8919261f694849998dd7
Steven Xu <stevenxxiu@gmail.com>
2022-03-22 23:09:18 +1100

    feat: add *Pre-commit* hooks; format `__init__.py`

--- __init__.py
+++ __init__.py
@@ -1,54 +1,58 @@
 # -*- coding: utf-8 -*-
 
-"""List and manage X11 windows.
+'''List and manage X11 windows.
 
-Synopsis: <filter>"""
+Synopsis: <filter>'''
 
 import subprocess
 from collections import namedtuple
-from albert import Item, ProcAction, iconLookup
 
-__title__ = "Window Switcher"
-__version__ = "0.4.5"
-__authors__ = ["Ed Perez", "manuelschneid3r", "dshoreman"]
-__exec_deps__ = ["wmctrl"]
+from albert import Item, ProcAction, iconLookup  # pylint: disable=import-error
 
-Window = namedtuple("Window", ["wid", "desktop", "wm_class", "host", "wm_name"])
 
-def handleQuery(query):
-    stripped = query.string.strip().lower()
-    if stripped:
-        results = []
-        for line in subprocess.check_output(['wmctrl', '-l', '-x']).splitlines():
-            win = Window(*parseWindow(line))
+__title__ = 'Window Switcher'
+__version__ = '0.4.5'
+__authors__ = ['Ed Perez', 'manuelschneid3r', 'dshoreman']
+__exec_deps__ = ['wmctrl']
 
-            if win.desktop == "-1":
-                continue
+Window = namedtuple('Window', ['wid', 'desktop', 'wm_class', 'host', 'wm_name'])
 
-            win_instance, win_class = win.wm_class.replace(' ', '-').split('.')
-            matches = [
-                win_instance.lower(),
-                win_class.lower(),
-                win.wm_name.lower()
-            ]
 
-            if any(stripped in match for match in matches):
-                iconPath = iconLookup(win_instance) or iconLookup(win_class.lower())
-                results.append(Item(id="%s%s" % (__title__, win.wm_class),
-                                    icon=iconPath,
-                                    text="%s  - <i>Desktop %s</i>" % (win_class.replace('-',' '), win.desktop),
-                                    subtext=win.wm_name,
-                                    actions=[ProcAction("Switch Window",
-                                                        ["wmctrl", '-i', '-a', win.wid] ),
-                                             ProcAction("Move window to this desktop",
-                                                        ["wmctrl", '-i', '-R', win.wid] ),
-                                             ProcAction("Close the window gracefully.",
-                                                        ["wmctrl", '-c', win.wid])]))
-        return results
-
-def parseWindow(line):
+def parse_window(line):
     win_id, desktop, rest = line.decode().split(None, 2)
     win_class, rest = rest.split('  ', 1)
     host, title = rest.strip().split(None, 1)
 
     return [win_id, desktop, win_class, host, title]
+
+
+def handleQuery(query):
+    stripped = query.string.strip().lower()
+    if not stripped:
+        return None
+    results = []
+    for line in subprocess.check_output(['wmctrl', '-l', '-x']).splitlines():
+        win = Window(*parse_window(line))
+
+        if win.desktop == '-1':
+            continue
+
+        win_instance, win_class = win.wm_class.replace(' ', '-').split('.')
+        matches = [win_instance.lower(), win_class.lower(), win.wm_name.lower()]
+
+        if any(stripped in match for match in matches):
+            icon_path = iconLookup(win_instance) or iconLookup(win_class.lower())
+            results.append(
+                Item(
+                    id=f'{__title__}{win.wm_class}',
+                    icon=icon_path,
+                    text=f'{win_class.replace("-", " ")}  - <i>Desktop {win.desktop}</i>',
+                    subtext=win.wm_name,
+                    actions=[
+                        ProcAction('Switch Window', ['wmctrl', '-i', '-a', win.wid]),
+                        ProcAction('Move window to this desktop', ['wmctrl', '-i', '-R', win.wid]),
+                        ProcAction('Close the window gracefully.', ['wmctrl', '-c', win.wid]),
+                    ],
+                )
+            )
+    return results
---
+++ readme.md
@@ -0,0 +1,10 @@
+# User Customized Window Switcher Extension
+# Install
+To install, copy or symlink this directory to `~/.local/share/albert/org.albert.extension.python/modules/window_switcher_user/`.
+
+# Development Setup
+To setup the project for development, run:
+
+    $ cd window_switcher_user/
+    $ npm install
+    $ pre-commit install --hook-type pre-commit --hook-type commit-msg
