f45c4d429df6897cb80ee21189e76bd99e59e06b
Steven Xu <stevenxxiu@gmail.com>
2025-08-24 19:26:49 +1000

    feat: work with *Sway*

--- __init__.py
+++ __init__.py
@@ -1,104 +1,122 @@
-from typing import Callable, ParamSpec
+import inspect
+import json
+import subprocess
+from dataclasses import dataclass
+from typing import Any, Callable, ParamSpec, override
 
-from albert import Action, Matcher, PluginInstance, StandardItem, TriggerQueryHandler
-from ewmh import EWMH
+from albert import (
+    Action,
+    Matcher,
+    PluginInstance,
+    Query,
+    StandardItem,
+    TriggerQueryHandler,
+)
 
 md_iid = '3.0'
-md_version = '1.4'
+md_version = '1.5'
 md_name = 'Window Switcher Steven'
 md_description = 'List and manage X11 windows'
 md_license = 'MIT'
 md_url = 'https://github.com/stevenxxiu/albert_window_switcher_steven'
 md_authors = ['@stevenxxiu']
-md_lib_dependencies = ['ewmh']
 
 
-WM_CLASS_TO_ICON_NAME: dict[tuple[str, str], str] = {
-    ('gimp-2.10', 'Gimp-2.10'): 'xdg:gimp',
-    ('jetbrains-clion', 'jetbrains-clion'): 'xdg:clion',
-    ('jetbrains-idea', 'jetbrains-idea'): 'xdg:intellij-idea-ultimate-edition',
-    ('jetbrains-pycharm', 'jetbrains-pycharm'): 'xdg:pycharm',
-    ('nomacs', 'Image Lounge'): 'xdg:org.nomacs.ImageLounge',
-    ('PDF Studio Pro', 'PDF Studio Pro'): 'xdg:pdfstudio',
-    ('subl', 'Subl'): 'xdg:sublime-text',
-    ('texmacs.bin', 'texmacs.bin'): 'xdg:TeXmacs',
-    ('vencorddesktop', 'VencordDesktop'): 'xdg:vencord-desktop',
-    ('vivaldi-stable', 'Vivaldi-stable'): 'xdg:vivaldi',
+APP_ID_TO_ICON_NAME: dict[str, str] = {
+    'texmacs': 'xdg:TeXmacs',
 }
 
 
-def get_icon_urls(win_instance: str, win_class: str) -> list[str]:
+def get_icon_urls(app_id: str) -> list[str]:
+    print(app_id)
     res = []
-    if (win_instance, win_class) in WM_CLASS_TO_ICON_NAME:
-        res = [WM_CLASS_TO_ICON_NAME[(win_instance, win_class)]]
-    return [*res, f'xdg:{win_instance}', f'xdg:{win_class.lower()}']
+    if app_id in APP_ID_TO_ICON_NAME:
+        res = [APP_ID_TO_ICON_NAME[app_id]]
+    return [*res, f'xdg:{app_id}']
+
+
+def switch_window(id: int) -> None:
+    _ = subprocess.call(['swaymsg', f'[con_id={id}]', 'focus'])
 
 
 Param = ParamSpec('Param')
+
+
+@dataclass
+class SwayTreeNode:
+    id: int
+    type: str
+    name: str
+    floating_nodes: list['SwayTreeNode']
+    nodes: list['SwayTreeNode']
+    pid: int | None
+    app_id: str | None
+
+    @classmethod
+    def from_dict(cls, env: dict[str, Any]):  # pyright: ignore[reportExplicitAny]
+        _ = env.setdefault('pid', None)
+        _ = env.setdefault('app_id', None)
+        if 'floating_nodes' in env:
+            env['floating_nodes'] = [SwayTreeNode.from_dict(node) for node in env['floating_nodes']]  # pyright: ignore[reportAny]
+        if 'nodes' in env:
+            env['nodes'] = [SwayTreeNode.from_dict(node) for node in env['nodes']]  # pyright: ignore[reportAny]
+        return cls(
+            **{  # pyright: ignore[reportAny]
+                k: v
+                for k, v in env.items()  # pyright: ignore[reportAny]
+                if k in inspect.signature(cls).parameters
+            }
+        )
 
 
 class Plugin(PluginInstance, TriggerQueryHandler):
     def __init__(self):
         PluginInstance.__init__(self)
         TriggerQueryHandler.__init__(self)
-        self.ewmh = EWMH()
 
+    @override
     def synopsis(self, _query: str) -> str:
         return 'filter'
 
+    @override
     def defaultTrigger(self):
         return 'w '
 
-    def with_flush(self, func: Callable[..., None]) -> Callable[..., None]:
-        def wrapper(*args, **kwargs) -> None:
-            func(*args, **kwargs)
-            self.ewmh.display.flush()
-
-        return wrapper
-
-    def handleTriggerQuery(self, query) -> None:
+    @override
+    def handleTriggerQuery(self, query: Query) -> None:
         matcher = Matcher(query.string)
 
-        cur_desktop = self.ewmh.getCurrentDesktop()
-        windows = self.ewmh.getClientList()
-        for window in windows:
-            assert window is not None
+        root_node = SwayTreeNode.from_dict(
+            json.loads(subprocess.check_output(['swaymsg', '--type', 'get_tree', '--raw']))  # pyright: ignore[reportAny]
+        )
+        stack: list[tuple[str | None, SwayTreeNode]] = [(None, root_node)]
+        items: list[StandardItem] = []
+        while stack:
+            workspace, cur_node = stack.pop()
+            if cur_node.type == 'workspace':
+                workspace = cur_node.name
+            if cur_node.pid is not None:
+                assert cur_node.app_id is not None
+                if matcher.match(cur_node.app_id) or matcher.match(cur_node.name):
+                    floating_text = ' (floating)' if cur_node.type == 'floating_con' else ''
+                    switch_window_call: Callable[[int], None] = lambda id_=cur_node.id: switch_window(id_)  # noqa: E731
+                    item = StandardItem(
+                        id=f'{md_name}/{cur_node.id}',
+                        text=f'{cur_node.app_id}{floating_text} - <i>Workspace {workspace}</i>',
+                        subtext=cur_node.name,
+                        iconUrls=get_icon_urls(cur_node.app_id),
+                        actions=[
+                            Action(
+                                f'{md_name}/switch/{cur_node.id}',
+                                'Switch Window',
+                                switch_window_call,
+                            ),
+                        ],
+                    )
+                    items.append(item)
 
-            if self.ewmh.getWmDesktop(window) == 0xFFFFFFFF:
-                continue
+            stack.extend([(workspace, child) for child in cur_node.nodes])
+            stack.extend([(workspace, child) for child in cur_node.floating_nodes])
 
-            wm_class = window.get_wm_class()
-            assert wm_class is not None
-            (win_instance, win_class) = wm_class
-
-            win_desktop = self.ewmh.getWmDesktop(window)
-
-            wm_name = self.ewmh.getWmName(window)
-            assert wm_name is not None
-            win_wm_name = wm_name.decode()
-
-            if matcher.match(win_instance) or matcher.match(win_class) or matcher.match(win_wm_name):
-                item = StandardItem(
-                    id=f'{md_name}/{window.id}',
-                    text=f'{win_class} - <i>Desktop {win_desktop}</i>',
-                    subtext=win_wm_name,
-                    iconUrls=get_icon_urls(win_instance, win_class),
-                    actions=[
-                        Action(
-                            f'{md_name}/switch/{window.id}',
-                            'Switch Window',
-                            self.with_flush(lambda window_=window: self.ewmh.setActiveWindow(window_)),
-                        ),
-                        Action(
-                            f'{md_name}/move_to_desktop/{window.id}',
-                            'Move window to this desktop',
-                            self.with_flush(lambda window_=window: self.ewmh.setWmDesktop(window_, cur_desktop)),
-                        ),
-                        Action(
-                            f'{md_name}/close/{window.id}',
-                            'Close the window gracefully',
-                            self.with_flush(lambda window_=window: self.ewmh.setCloseWindow(window_)),
-                        ),
-                    ],
-                )
-                query.add(item)
+        for item in items:
+            query.add(item)  # pyright: ignore[reportUnknownMemberType]
