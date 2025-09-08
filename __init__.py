650ae268ab9385731d4dec1199bd30d4f22878cc
Steven Xu <stevenxxiu@gmail.com>
2025-09-08 17:52:19 +1000

    refactor: use *i3ipc-python*
    - Use `self.id()`.
    - Use `query.add(items)`.

--- __init__.py
+++ __init__.py
@@ -1,8 +1,5 @@
-import inspect
-import json
-import subprocess
-from dataclasses import dataclass
-from typing import Any, Callable, ParamSpec, override
+import asyncio
+from typing import Callable, Self, cast, override
 
 from albert import (
     Action,
@@ -12,6 +9,8 @@ from albert import (
     StandardItem,
     TriggerQueryHandler,
 )
+from i3ipc.aio import Connection  # pyright: ignore[reportPrivateLocalImportUsage]
+from i3ipc.aio import connection
 
 md_iid = '3.0'
 md_version = '1.5'
@@ -20,6 +19,7 @@ md_description = 'List and manage Sway windows'
 md_license = 'MIT'
 md_url = 'https://github.com/stevenxxiu/albert_window_switcher_steven'
 md_authors = ['@stevenxxiu']
+md_lib_dependencies = ['i3ipc']
 
 
 APP_ID_TO_ICON_NAME: dict[str, str] = {
@@ -28,49 +28,29 @@ APP_ID_TO_ICON_NAME: dict[str, str] = {
 
 
 def get_icon_urls(app_id: str) -> list[str]:
-    print(app_id)
     res = []
     if app_id in APP_ID_TO_ICON_NAME:
         res = [APP_ID_TO_ICON_NAME[app_id]]
     return [*res, f'xdg:{app_id}']
 
 
-def switch_window(id: int) -> None:
-    _ = subprocess.call(['swaymsg', f'[con_id={id}]', 'focus'])
-
-
-Param = ParamSpec('Param')
-
-
-@dataclass
-class SwayTreeNode:
-    id: int
+class SwayTreeNode(connection.Con):
     type: str
-    name: str
-    floating_nodes: list['SwayTreeNode']
-    nodes: list['SwayTreeNode']
-    pid: int | None
-    app_id: str | None
+    name: str  # pyright: ignore[reportUninitializedInstanceVariable]
+    pid: int | None  # pyright: ignore[reportUninitializedInstanceVariable]
+    app_id: str | None  # pyright: ignore[reportUninitializedInstanceVariable]
 
-    @classmethod
-    def from_dict(cls, env: dict[str, Any]):  # pyright: ignore[reportExplicitAny]
-        _ = env.setdefault('pid', None)
-        _ = env.setdefault('app_id', None)
-        if 'floating_nodes' in env:
-            env['floating_nodes'] = [SwayTreeNode.from_dict(node) for node in env['floating_nodes']]  # pyright: ignore[reportAny]
-        if 'nodes' in env:
-            env['nodes'] = [SwayTreeNode.from_dict(node) for node in env['nodes']]  # pyright: ignore[reportAny]
-        return cls(
-            **{  # pyright: ignore[reportAny]
-                k: v
-                for k, v in env.items()  # pyright: ignore[reportAny]
-                if k in inspect.signature(cls).parameters
-            }
-        )
+    @override
+    def workspace(self) -> Self:
+        return self
+
+    @override
+    def command(self, cmd: str) -> None:
+        pass
 
 
 class Plugin(PluginInstance, TriggerQueryHandler):
-    def __init__(self):
+    def __init__(self) -> None:
         PluginInstance.__init__(self)
         TriggerQueryHandler.__init__(self)
 
@@ -79,45 +59,44 @@ class Plugin(PluginInstance, TriggerQueryHandler):
         return 'filter'
 
     @override
-    def defaultTrigger(self):
+    def defaultTrigger(self) -> str:
         return 'w '
+
+    async def asyncHandleTriggerQuery(self, query: Query) -> None:
+        sway = await Connection().connect()
+        matcher = Matcher(query.string)
+
+        items = []
+        tree = await sway.get_tree()
+        for node in tree.descendants():
+            node = cast(SwayTreeNode, node)
+            if node.pid is None:
+                continue
+            if not (matcher.match(node.name) or (node.app_id is not None and matcher.match(node.app_id))):
+                continue
+            workspace_name = node.workspace().name
+            floating_text: str = ' (floating)' if node.type == 'floating_con' else ''
+            switch_window_call: Callable[[SwayTreeNode], None] = lambda node_=node: asyncio.run(node_.command('focus'))  # noqa: E731  # pyright: ignore[reportUnknownLambdaType, reportArgumentType]
+            icon_urls = []
+            if node.app_id is not None:
+                icon_urls = get_icon_urls(node.app_id)
+            items.append(  # pyright: ignore[reportUnknownMemberType]
+                StandardItem(
+                    id=self.id(),
+                    text=f'{node.name}{floating_text} - <i>Workspace {workspace_name}</i>',
+                    subtext=node.app_id or '',
+                    iconUrls=icon_urls,
+                    actions=[
+                        Action(
+                            self.id(),
+                            'Switch Window',
+                            switch_window_call,
+                        ),
+                    ],
+                )
+            )
+        query.add(items)  # pyright: ignore[reportUnknownMemberType, reportUnknownArgumentType]
 
     @override
     def handleTriggerQuery(self, query: Query) -> None:
-        matcher = Matcher(query.string)
-
-        root_node = SwayTreeNode.from_dict(
-            json.loads(subprocess.check_output(['swaymsg', '--type', 'get_tree', '--raw']))  # pyright: ignore[reportAny]
-        )
-        stack: list[tuple[str | None, SwayTreeNode]] = [(None, root_node)]
-        items: list[StandardItem] = []
-        while stack:
-            workspace, cur_node = stack.pop()
-            if cur_node.type == 'workspace':
-                workspace = cur_node.name
-            if cur_node.pid is not None:
-                assert cur_node.app_id is not None
-                if matcher.match(cur_node.app_id) or matcher.match(cur_node.name):
-                    floating_text = ' (floating)' if cur_node.type == 'floating_con' else ''
-                    switch_window_call: Callable[[int], None] = lambda id_=cur_node.id: switch_window(id_)  # noqa: E731
-                    item = StandardItem(
-                        id=f'{md_name}/{cur_node.id}',
-                        text=f'{cur_node.app_id}{floating_text} - <i>Workspace {workspace}</i>',
-                        subtext=cur_node.name,
-                        iconUrls=get_icon_urls(cur_node.app_id),
-                        actions=[
-                            Action(
-                                f'{md_name}/switch/{cur_node.id}',
-                                'Switch Window',
-                                switch_window_call,
-                            ),
-                        ],
-                    )
-                    items.append(item)
-
-            stack.extend([(workspace, child) for child in cur_node.nodes])
-            stack.extend([(workspace, child) for child in cur_node.floating_nodes])
-
-        # Reverse to match the window display order
-        for item in reversed(items):
-            query.add(item)  # pyright: ignore[reportUnknownMemberType]
+        asyncio.run(self.asyncHandleTriggerQuery(query))
